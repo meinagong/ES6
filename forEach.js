7d48edc0fcc65a1c9d5542714e89ac445d10eb1c
meinagong <meinagong@gmail.com>
2021-06-17 19:54:13 +0800

    added forEach file

---
+++ forEach.js
@@ -0,0 +1,18 @@
+//1.create an array of numbers
+//2.create a variable to hold the sum
+//3.loop over the array, incrementing the sum variable
+//4.print the sum variable
+
+const numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+numbers.forEach(num => {
+  sum += num;
+});
+console.log(sum);
+
+
+
+
+// const colors = ['red', 'blue', 'green'];
+
+// colors.forEach(color => console.log(color));
